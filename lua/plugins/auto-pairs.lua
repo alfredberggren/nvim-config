bb09858447b07d920c14f4b605c1510bd105075b
Alfred Berggren <alfred.berggren@gmail.com>
2024-05-22 19:59:11 +0200

    initial setup

---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,10 @@
+return {
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = true,
+  opts = {
+    disable_in_macro = true
+  }
+	-- use opts = {} for passing setup options
+	-- this is equalent to setup({}) function
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set nu! rnu!")
+vim.cmd("set colorcolumn=80")
+vim.cmd("highlight ColorColumn ctermbg=237")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
