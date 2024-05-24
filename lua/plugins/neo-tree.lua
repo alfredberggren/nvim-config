759f4b76c7a74abd8edd30c93e30a489b4bd78b2
Alfred Berggren <alfred.berggren@gmail.com>
2024-05-24 17:27:34 +0200

    add venv-selector, update neotree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return   {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree toggle position=float<CR>', {})
   end
 }
