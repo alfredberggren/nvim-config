4135b2c8a462daf1c9bf4b610d6ab5e9f2aeb12c
Alfred Berggren <alfred.berggren@gmail.com>
2024-06-02 07:38:16 +0200

    add fastwrap

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -2,9 +2,10 @@ return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
 	config = true,
-  opts = {
-    disable_in_macro = true
-  }
+	opts = {
+		fast_wrap = {},
+		disable_in_macro = true,
+	},
 	-- use opts = {} for passing setup options
 	-- this is equalent to setup({}) function
 }
