c634ddf0b98eb3845e6959d7d7cc28d24290ec40
Alfred Berggren <alfred.berggren@gmail.com>
2024-06-09 21:42:57 +0200

    add keymap for navigating buffers, formatting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,7 +7,12 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+-- Navigating windows
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Navigating buffers
+vim.keymap.set('n', '<C-Tab>', ':bnext<CR>')
+vim.keymap.set('n', '<C-d>', ':bdelete<CR>')
