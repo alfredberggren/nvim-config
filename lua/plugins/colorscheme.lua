acd475db9a0f1ca20484a29bd0c3d8b1cfbc5c5a
Alfred Berggren <alfred.berggren@gmail.com>
2024-06-10 10:15:53 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-	-- "catppuccin/nvim",
-	-- name = "catppuccin",
-	-- priority = 1000,
-	-- config = function()
-	-- 	vim.cmd.colorscheme("catppuccin")
-	-- end,
-	"rebelot/kanagawa.nvim",
-	config = function()
-		vim.cmd.colorscheme("kanagawa-dragon")
-	end,
+  -- "catppuccin/nvim",
+  -- name = "catppuccin",
+  -- priority = 1000,
+  -- config = function()
+  -- 	vim.cmd.colorscheme("catppuccin")
+  -- end,
+  "rebelot/kanagawa.nvim",
+  config = function()
+    vim.cmd("colorscheme kanagawa-dragon")
+  end,
 }
