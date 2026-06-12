9b2d35329834753459c6de8fdd0121beb376c1a6
Alfred Berggren <alfred.berggren@destiny.eu>
2026-06-12 18:17:03 +0200

    add clang

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,69 +1,66 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		-- 	config = function()
-		-- 	require("mason-lspconfig").setup({
-		-- 		ensure_installed = { "lua_ls", "pylsp" },
-		-- 	})
-		-- end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    -- 	config = function()
+    -- 	require("mason-lspconfig").setup({
+    -- 		ensure_installed = { "lua_ls", "pylsp" },
+    -- 	})
+    -- end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			-- LUA LSP CONFIG
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      -- LUA LSP CONFIG
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- PYLSP CONFIG
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-				settings = {
-					pylsp = {
-						plugins = {
-							jedi_completion = {
-								-- include_class_objects = true,
-								-- include_function_objects = true,
-                fuzzy = true,
-							},
-							autopep8 = {
-								enabled = false,
-							},
-							mccabe = {
-								enabled = false,
-							},
-							pycodestyle = {
-								enabled = false,
-							},
-							pyflakes = {
-								enabled = false,
-							},
-							yapf = {
-								enabled = false,
-							},
-						},
-					},
-				},
-			})
+      -- PYLSP CONFIG
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+        settings = {
+          pylsp = {
+            plugins = {
+              -- jedi_completion = {fuzzy = true},
+              -- jedi_completion = {eager=true},
+              jedi_completion = {
+                include_params = true,
+              },
+              jedi_signature_help = { enabled = true },
+              jedi = {
+                extra_paths = { "~/projects/work_odoo/odoo14", "~/projects/work_odoo/odoo14" },
+                -- environment = {"odoo"},
+              },
+              pyflakes = { enabled = true },
+              pylint = {args = {'--ignore=E501,E231', '-'}, enabled=true, debounce=200},
+              pylsp_mypy = { enabled = false },
+              pycodestyle = {
+                enabled = true,
+                ignore = { "E501", "E231" },
+                maxLineLength = 120,
+              },
+              yapf = { enabled = true },
+            },
+          },
+        },
+      })
 
-			-- RUFF CONFIG (LINTING AND FORMATTING)
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-			})
+      -- RUFF CONFIG (LINTING AND FORMATTING)
+      -- lspconfig.ruff.setup({})
 
       -- HTML
       lspconfig.html.setup({
@@ -75,10 +72,15 @@ return {
         capabilities = capabilities,
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      -- CLANG
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
