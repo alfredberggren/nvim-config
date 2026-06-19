bce4e17a3b8a694479dec8c56dfe6ead36eb14d1
Alfred Berggren <alfred.berggren@destiny.eu>
2026-06-19 20:46:45 +0200

    Remove all config

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.api.nvim_create_user_command('Cheatsheet', function()
   -- Define path to your cheat sheet markdown file
   local filepath = vim.fn.expand('~/.config/nvim/cheatsheet.md')
@@ -55,4 +42,3 @@ vim.api.nvim_create_user_command('Cheatsheet', function()
 end, {})
 
 require("vim-options")
-require("lazy").setup("plugins")
